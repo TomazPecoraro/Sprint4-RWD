09d9cfc26d8ef7c9d49e57754a6a1e737a885534
Alexandreportugal <xan.portugal121@gmail.com>
2023-11-13 19:08:16 -0300

    resultados ok

---
+++ src/pages/Vistoria/Resultados/Aprovado.jsx
@@ -0,0 +1,26 @@
+import '../../../app/globals.css'
+import Header from '../../../components/Cabecalho/cabecalho';
+import Link from 'next/link';
+import styles from './resultados.module.css';
+
+export default function Aprovado() {
+    return (
+        <div>
+            <header>
+                <Header/>
+            </header>
+            <main>
+                <section>
+                <h1 className={styles.fundobranco}>Conclusão</h1>
+                </section>
+                <section>
+                    <p>Seu resultado: Aprovado!</p>
+                    <p>Sua bike é real, e é otima!</p>
+                </section> 
+                <section>
+                    <Link href="../../../">Voltar para Home</Link>
+                </section>
+            </main>
+        </div>
+    )
+}
