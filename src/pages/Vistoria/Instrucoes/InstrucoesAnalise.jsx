5aa0889ca142909f207e639905f4930a0765ace0
Alexandreportugal <xan.portugal121@gmail.com>
2023-11-13 19:07:08 -0300

    instrucoes ok

---
+++ src/pages/Vistoria/Instrucoes/InstrucoesAnalise.jsx
@@ -0,0 +1,33 @@
+import '../../../app/globals.css'
+import Header from '../../../components/Cabecalho/cabecalho';
+import Link from 'next/link';
+import Image from "next/image";
+import styles from './instrucoes.module.css';
+
+export default function InstrucoesAnalise() {
+    return (
+        <div>
+            <header>
+                <Header/>
+            </header>
+            <main>
+                <section>
+                <h1 className={styles.fundobranco}>Concluído</h1>
+                </section>
+                <section>
+                    <p>Pronto! Enviado</p>
+                </section> 
+                <section>
+                    <p>Suas fotos estão em análise!</p>
+                </section> 
+                <section>
+                    <Image src="/23.png" width={246}height={8}/>
+                    <p>99%</p>
+                </section>
+                <section>
+                    <Link href="../Fim/fimvistoria">Próximo</Link>
+                </section>
+            </main>
+        </div>
+    )
+}
